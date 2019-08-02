c21e7450214571daa3c3d35fcbebf3f121c7d0aa
John Desrosiers <john.desro@gmail.com>
2019-08-01 17:43:04 -0700

    Photo sphere viewer example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import logo from './logo.svg'
+import SphereComponent from './components/SphereComponent'
 import './App.css'
 
-function App() {
-  return <div className="App">Hi</div>
+const panorama1 = 'http://reznik.lt/wp-content/uploads/2017/09/preview3000.jpg'
+const panorama2 =
+  'https://photo-sphere-viewer.js.org/assets/Bryce-Canyon-National-Park-Mark-Doliner.jpg'
+
+class App extends React.Component {
+  state = {
+    panorama: panorama1
+  }
+
+  togglePanorama = () => {
+    let { panorama } = this.state
+    panorama = panorama === panorama1 ? panorama2 : panorama1
+    this.setState({ panorama })
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div className="container">
+        <button
+          onClick={this.togglePanorama}
+          className="btn btn-block btn-lg btn-primary mt-3"
+        >
+          Toggle Panorama
+        </button>
+        <div className="m-5">
+          <SphereComponent panorama={this.state.panorama} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ src/components/SphereComponent/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import * as Sphere from 'photo-sphere-viewer'
+import 'photo-sphere-viewer/dist/photo-sphere-viewer.min.css'
+
+export default class SphereComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.divStyle = {
+      width: '100%',
+      height: '600px'
+    }
+    this.sphereDiv = element => {
+      this.photoSphereViewer = element
+    }
+    this.sphereDiv.appendChild = elem => {
+      this.subDiv.appendChild(elem)
+    }
+  } 
+
+  componentDidMount() {
+    this.psv = Sphere({
+      parent: this,
+      container: this.sphereDiv,
+      panorama: this.props.panorama,
+      navbar: ['autorotate', 'zoom', 'fullscreen']
+    })
+  }
+
+  // when the component updates
+  componentDidUpdate(prevProps) {
+    // check if there is a new value for the panorama prop
+    if (prevProps.panorama !== this.props.panorama) {
+      // load a new panoramic image in the viewer
+      this.psv.setPanorama(this.props.panorama)
+    }
+  }
+
+  render() {
+    return (
+      <div style={this.divStyle} ref={this.sphereDiv} id="viewer">
+        <div ref={node => (this.subDiv = node)} style={this.divStyle} />
+      </div>
+    )
+  }
+}
